8a6d77f07de080dcedfff3ba60152e99abc99b1b
jiavu <39624614+jiavu@users.noreply.github.com>
2019-06-20 23:55:23 +0200

    Basic Component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { NewsControlComponent } from './news-control/news-control.component';
+import { NewsSearchComponent } from './news-control/news-search/news-search.component';
+import { NewsFilterComponent } from "./news-control/news-filter/news-filter.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    NewsControlComponent,
+    NewsSearchComponent,
+    NewsFilterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/news-control/news-filter/news-filter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component( {
+  selector: 'app-news-filter',
+  templateUrl: './news-filter.component.html',
+  styleUrls: ['./news-filter.component.scss']
+})
+export class NewsFilterComponent {
+}
